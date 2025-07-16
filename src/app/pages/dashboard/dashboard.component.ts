57440ff194a325b5669770ebccb6523a13920d2a
giuliano300 <valente.giuliano11@gmail.com>
2025-07-16 11:07:49 +0200

    add tour

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ShepherdService } from 'angular-shepherd';
+import { Placement as PopperPlacement } from '@popperjs/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,7 +14,52 @@ export class DashboardComponent {
 
   currentModalRef: any;
 
-  constructor(private modalService: NgbModal, private router: Router) {}
+  constructor(private modalService: NgbModal, private router: Router, private shepherdService: ShepherdService) {}
+
+
+  ngOnInit() {
+    //if(!localStorage.getItem("tour")){
+      this.startTour();
+      //localStorage.setItem("tour", "true");
+    //}
+  }
+
+  startTour() {
+
+  const steps = [
+      {
+        id: 'dashboard',
+        text: 'Questa è la dashboard principale.',
+        attachTo: {
+          element: '.test',
+          on: 'bottom' as PopperPlacement 
+        },
+        buttons: [
+          { text: 'Avanti', action: () => this.shepherdService.next() }
+        ]
+      },
+      {
+        id: 'dashboard',
+        text: 'Questa è la dashboard principale.',
+        attachTo: {
+          element: '.test2',
+          on: 'top' as PopperPlacement 
+        },
+        buttons: [
+          { text: 'fine', action: () => this.shepherdService.complete() }
+        ]
+      }
+
+    ];
+    this.shepherdService.defaultStepOptions = {
+      scrollTo: true,
+      cancelIcon: { enabled: true },
+      classes: 'shepherd-theme-arrows'
+    };
+
+    this.shepherdService.addSteps(steps);
+    this.shepherdService.start();
+  }
 
   // Metodo per aprire il modal e salvare il riferimento
   openModal(content: any) {
